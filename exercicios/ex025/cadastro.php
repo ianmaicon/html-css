cf8efeb7a03b368616f6e0ff81005ab965d0991d
ianmaicon <80468938+ianmaicon@users.noreply.github.com>
2022-08-23 18:05:07 -0400

    .

---
+++ exercicios/ex025/cadastro.php
@@ -0,0 +1,2 @@
+Cadastro solicitado!
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas saepe perspiciatis laboriosam molestiae nulla dolores, accusamus at sed ut ea. Neque cupiditate rem esse tenetur dolorem dolore maxime molestias ratione.
