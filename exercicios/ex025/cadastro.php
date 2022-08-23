de67434dd28e777f538c4ef207820f3bc83011c4
ianmaicon <80468938+ianmaicon@users.noreply.github.com>
2022-08-23 19:02:13 -0400

    deletei o cadastro

--- exercicios/ex025/cadastro.php
+++
@@ -1,2 +0,0 @@
-Cadastro solicitado!
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas saepe perspiciatis laboriosam molestiae nulla dolores, accusamus at sed ut ea. Neque cupiditate rem esse tenetur dolorem dolore maxime molestias ratione.
